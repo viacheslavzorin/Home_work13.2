0fcf90dda7bc30a9f80a0a7776df7c443146689d
viacheslavzorin <zorin-viacheslav@rambler.ru>
2023-02-28 16:01:59 +0300

    Домашка 13.2

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from channel import Channel
-
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
-channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
+import json
+# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
 
 vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 vdud.print_info()
+#print(vdud.title)
+print(vdud.id)
+print(vdud.title)
+print(vdud.url)
+print(vdud.description)
+print(vdud.subscriberCount)
+print(vdud.videoCount)
+print(vdud.viewCount)
+vdud.channel_id= 'Новое название'
+#print(vdud.channel_id)
+#print(Channel.get_service)
+print(vdud.get_service())
+vdud.to_json()
